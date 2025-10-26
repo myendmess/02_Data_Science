3e5e37f3b82310766faa9eea3f13898944a58c7e
myendmess <boujnanemohamed23@gmail.com>
2025-10-26 10:04:34 +0100

    Update

---
+++ Python/01_Functions.py
@@ -0,0 +1,9 @@
+
+# Functions: A group of related statements to perform a task and return a value
+
+def to_celsius(x):
+   '''Convert Fahrenheit to Celsius'''
+   return (x-32) * 5/9
+
+to_celsius(75)
+print(to_celsius(75))
---
+++ Python/02_Conditions.py
@@ -0,0 +1,12 @@
+
+
+# Functions: A group of related statements to perform a task and return a value
+
+number = -4
+
+if number > 0:
+   print('Number is positive.')
+elif number == 0:
+   print('Number is zero.')
+else:
+   print('Number is negative.')
